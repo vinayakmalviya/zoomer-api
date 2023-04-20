06e6c3354e2a51f363947d01d513a62d1f07cfe7
Vinayak Malviya <vinayak.malviya@somaiya.edu>
2023-04-20 23:56:29 +0530

    :heavy_plus_sign: add deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let routes = warp::any().map(|| "Hello world!");
+
+    warp::serve(routes).run(([0, 0, 0, 0], 4000)).await;
 }
