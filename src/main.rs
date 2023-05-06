8841e2ec702b58f7f38cee74d6cee4261ea14dd4
Vinayak Malviya <vinayak.malviya@somaiya.edu>
2023-05-06 23:43:30 +0530

    :card_file_box: setup db connection

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod errors;
 mod rooms;
 
+use std::env;
+
+use dotenv::dotenv;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 use warp::Filter;
 
 use errors::handle_rejection;
@@ -8,12 +12,41 @@ use rooms::rooms_filter;
 
 #[tokio::main]
 async fn main() {
+    // Load env variables
+    dotenv().ok();
+
+    // Connect db
+    let db_string = env::var("DB_STRING").expect("Missing env var: DB_STRING");
+    let db_pool = connect_to_db(&db_string).await;
+
     // API routes
     let initial_route = warp::get()
         .and(warp::path::end())
         .map(|| "Zoomer API active");
 
-    let routes = initial_route.or(rooms_filter()).recover(handle_rejection);
+    let routes = initial_route
+        .or(rooms_filter(db_pool.clone()))
+        .recover(handle_rejection);
 
     warp::serve(routes).run(([0, 0, 0, 0], 4000)).await;
+}
+
+pub type DBPool = Pool<Postgres>;
+
+async fn connect_to_db(db_string: &str) -> DBPool {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(db_string)
+        .await;
+
+    match pool {
+        Ok(db) => db,
+        Err(e) => panic!("Couldn't connect to the database!: {}", e),
+    }
+}
+
+pub fn with_db(
+    db: Pool<Postgres>,
+) -> impl Filter<Extract = (Pool<Postgres>,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || db.clone())
 }
--- src/rooms.rs
+++ src/rooms.rs
@@ -1,33 +1,39 @@
 use serde::{Deserialize, Serialize};
 use warp::Filter;
 
+use crate::{with_db, DBPool};
+
 #[derive(Deserialize, Serialize)]
 struct Test {
     abcd: String,
     key2: String,
 }
 
-pub fn rooms_filter() -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone
-{
+pub fn rooms_filter(
+    db_pool: DBPool,
+) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
     let rooms_base = warp::path("rooms");
 
     let all_rooms = rooms_base
         .and(warp::get())
         .and(warp::path::end())
-        .map(|| "All rooms route");
+        .and(with_db(db_pool.clone()))
+        .map(|_db: DBPool| "All rooms route");
 
     let single_room = rooms_base
         .and(warp::get())
         .and(warp::path::param())
         .and(warp::path::end())
-        .map(|room_id: String| format!("Single room data {}", room_id));
+        .and(with_db(db_pool.clone()))
+        .map(|room_id: String, _db: DBPool| format!("Single room data {}", room_id));
 
     let new_room = rooms_base
         .and(warp::post())
         .and(warp::path("new"))
         .and(warp::path::end())
         .and(warp::body::json())
-        .map(|body: Test| warp::reply::json(&body));
+        .and(with_db(db_pool.clone()))
+        .map(|body: Test, _db: DBPool| warp::reply::json(&body));
 
     all_rooms.or(single_room).or(new_room)
 }
